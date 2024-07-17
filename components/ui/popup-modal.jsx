160048aaa52f2951d605016b64f6fbb4e3bacac7
shashank01sj <shashank@sportjacks.com>
2024-07-17 15:59:54 +0530

    trigerring modal function

--- components/ui/popup-modal.jsx
+++ components/ui/popup-modal.jsx
@@ -2,7 +2,7 @@
 
 import { cn } from "@/lib/utils"
 import { AnimatePresence, motion } from "framer-motion"
-import {React, useEffect, useRef } from "react"
+import {React, useEffect, useRef, useContext, useState } from "react"
 
 const ModalContext = createContext();
 
@@ -14,6 +14,38 @@ export const ModalPopup = ({children})=>{
         <ModalContext.Provider value={{open,setOpen}}>
             {children}
         </ModalContext.Provider>
+    )
+
+}
+
+export const useModal=()=>{
+    const context = useContext(ModalContext);
+    if(!context){
+        throw new Error("useModal must be used within a ModalPopup");
+    }
+    return context;
+}
+
+export function Modal({children}){
+    return <ModalPopup>
+        {children}
+    </ModalPopup>
+}
+    
+
+export const ModalTrigger = ({children,className})=>{
+    const {setOpen} = useModal();
+
+    return (
+      <button
+      className={cn(
+        "px-4 py-2 rounded-md text-black dark:text-white text-center relative overflow-hidden",
+        className
+      )}
+      onClick={()=>setOpen(true)}
+      >
+        {children}
+      </button>
     )
 
 }
